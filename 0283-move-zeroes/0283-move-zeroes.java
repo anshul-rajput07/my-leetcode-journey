f71841a697e211422004d5b3de1bcf9c435dbf7e
Anshul rajput <theanshulrajput@gmail.com>
2026-09-01 12:33:38 +0530

    Time: 2 ms (91.89%), Space: 47.9 MB (38.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,8 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i = 0;
-        while(i<nums.length && nums[i]!=0){
-                i++;
-        }
-        int j = i + 1;
-        while(i<nums.length && j < nums.length){
-            
+        int j = 0;
+        while(j < nums.length){
             if(nums[j] != 0){
                 int temp = nums[j];
                 nums[j] = 0;
@@ -14,6 +10,9 @@ class Solution {
                 i++;
             }
             j++;
+        }
+        while(j<nums.length){
+            nums[j] = 0 ;
         }
        
     }
